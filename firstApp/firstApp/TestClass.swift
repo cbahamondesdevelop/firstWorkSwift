a0aff73f4aa63f86da7f6752c00ca66c2b510388
c.bahamondes.quiroga <c.bahamondes.quiroga@accenture.com>
2022-09-08 15:34:52 -0400

    ADD: Se implementa instalacion de SwiftLint

---
+++ firstApp/firstApp/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  firstApp
+//
+//  Created by c.bahamondes.quiroga on 08-09-22.
+//
+
+import Foundation
+// swiftlint:enable all
+class TestClass {
+    
+    let persona :   String  = "Persona1"
+}
